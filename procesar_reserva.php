43f9b650f4848a04496262783bebd5c2530b9b01
holiwiscaquita <133812647+holiwiscaquita@users.noreply.github.com>
2024-10-22 09:02:05 -0300

    Update procesar_reserva.php

--- procesar_reserva.php
+++ procesar_reserva.php
@@ -3,18 +3,15 @@ $host = getenv('ep-lingering-resonance-a5wydir9.us-east-2.aws.neon.tech');
 $dbname = getenv('Comedor_escolar');
 $user = getenv('Comedor_escolar_owner');
 $password = getenv('IfJQNh60nrGW');
-
 $conn = new mysqli($host, $user, $password, $dbname);
-if (!$conn->connect_error) {
+if ($conn->connect_error) {
     die("Error de conexion: " . $conn->connect_error);
 }
 $nombre = $_POST['nombre'];
 $curso = $_POST['curso'];
 $horario = $_POST['horario'];
 $email = $_POST['email'];
-
 $sql = "INSERT INTO reservas (nombre, curso, horario, email) VALUES ('$nombre', '$curso', '$horario', '$email')";
-
 if ($conn->query($sql) === TRUE) {
     echo "Reserva registrada exitosamente.";
 } else {
